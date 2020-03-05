62c78cd9616d19fe0510f89b9f70310b231a04b5
kindziora@live.de <kindziora@live.de>
2020-03-05 18:25:47 +0100

    added new stuff

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ program.parse(process.argv);
 console.log(program.opts());
 
 var sourceDir = path.join(__dirname, "./" + program.type);
-var destinationDir = "./" + program.name;
+var destinationDir = program.name;
 
 if (!fs.existsSync(destinationDir)) {
     fs.mkdirSync(destinationDir, { recursive: true });
