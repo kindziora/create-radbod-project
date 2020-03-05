52f0c5dff2ccabfd1d6151c858cf4bfcb44a0764
kindziora@live.de <kindziora@live.de>
2020-03-05 18:23:08 +0100

    added new stuff

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const program = require('commander');
 const fs = require('fs');
 const fs_Extra = require('fs-extra');
 const path = require('path');
-
 let projectName = "my-project";
 let projectType = "es2015";
 
@@ -18,7 +17,7 @@ program.parse(process.argv);
 console.log(program.opts());
 
 var sourceDir = path.join(__dirname, "./" + program.type);
-var destinationDir ="./" + program.name;
+var destinationDir = "./" + program.name;
 
 if (!fs.existsSync(destinationDir)) {
     fs.mkdirSync(destinationDir, { recursive: true });
@@ -32,5 +31,21 @@ fs_Extra.copy(sourceDir, destinationDir, function (err) {
         throw err;
     } else {
         console.log("success!");
+
+
+        var exec = require('child_process').exec;
+
+        let cp = exec('npm install ' + destinationDir).stderr.pipe(process.stderr);
+
+        cp.on("exit", function (code, signal) {
+            console.log("Exited", { code: code, signal: signal });
+        });
+
+        cp.on("error", console.error.bind(console));
+
     }
+
+
+
 });
+
