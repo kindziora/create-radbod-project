75d33625d4476d7e3bf9f3bfcbddf5369f583e6a
kindziora@live.de <kindziora@live.de>
2020-03-10 18:06:24 +0100

    filter for mdb jobs

--- es2015/public/build/dev/app.js
+++ es2015/public/build/dev/app.js
@@ -3,11 +3,12 @@ import {home} from "./page/home.js";
 
 let buildApp = new window.radbod.app();
 
-console.log(buildApp);
-
 let compo = buildApp.mountComponent(
     "home", home,
     (stores, data, component)=>  {
         console.log(stores, data, component);
+
+        document.querySelector('#section').replaceWith(component.$el);
+ 
     }
 );
