07103679d41e035602e6b3eca083445fb7dc2f1e
kindziora <kindziora@live.de>
2021-02-25 22:56:01 +0100

    adding new starter

---
+++ iso-strapi-tail-mdb/config/client.dev.js
@@ -0,0 +1,28 @@
+export let environment = {
+    path : function(){
+        return window.location.pathname;
+    },
+    data_loader: {
+        find(query, onResultCallback) {
+
+            setTimeout(() => onResultCallback.call({ dataH: {} }, {
+                name: "AK TODOS c asdfsdf sdf",
+                tab: "all",
+                items: [{
+                    id: 0,
+                    label: "Testdaten1",
+                    checked: true
+                }, {
+                    id: 1,
+                    label: "Testdaten2",
+                    checked: false
+                }, {
+                    id: 2,
+                    label: "Testdaten3",
+                    checked: false
+                }]
+            }), 10);
+
+        }
+    }
+};
---
+++ iso-strapi-tail-mdb/config/server.dev.js
@@ -0,0 +1,20 @@
+export let environment = {
+
+    data_loader: {
+        find(options, cb) {
+            setTimeout(() => cb.call({ dataH: {} }, { name: "test load asynchronous server",    items: [{
+                id: 0,
+                label: "Testdaten1 server",
+                checked: false
+            }, {
+                id: 1,
+                label: "Testdaten2 server",
+                checked: false
+            }, {
+                id: 2,
+                label: "Testdaten3 server",
+                checked: false
+            }]}), 0);
+        }
+    }
+};
