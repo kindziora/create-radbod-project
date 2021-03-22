18709c5249b085fba200499a2450e4521bca73a3
kindziora <kindziora@live.de>
2021-03-22 22:28:49 +0100

    select bugfix

--- iso-strapi-tail-mdb/public/build/dev/middleware/validator/email.js
+++ iso-strapi-tail-mdb/public/build/dev/middleware/validator/email.js
@@ -1,6 +1,6 @@
 export const mail_validator = function (value) {
     let re = /^[äöüßa-zA-Z0-9._-]+@[äöüßa-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    let isValid = !(value == '' || !re.test(value.toLowerCase()));
+    let isValid = !(value == '' || value && !re.test(value.toLowerCase()));
 
     return {
         isValid: isValid,
